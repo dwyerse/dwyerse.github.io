16c1eaa0885db7f8360e25c7dcf5d5029f12b382
Seamus Dwyer <dwyers1@tcd.ie>
2020-11-22 14:48:33 +0000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./profile.jpeg";
+import "./App.css";
+import Card from "./Card.js";
+import catNotesImg from "./cat-notes.png";
+import floodPlainsImg from "./flood-plains.png";
+import wordBridgesImg from "./word-bridges.webp";
+import logTablesImg from "./log-tables.webp";
+import formulaeImg from "./formulae.webp";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3>Seamus Dwyer</h3>
       </header>
+      <h1 style={{ color: "#343b47", fontWeight: 300 }}>Projects</h1>
+      <div className="projects">
+        <Card
+          image={catNotesImg}
+          link={"www.google.com"}
+          title={"Cat Notes"}
+          year={"2020"}
+          subtitle={"Cat themed notes app written using React Native"}
+        />
+        <Card
+          image={floodPlainsImg}
+          link={"https://simmer.io/@Sheepdog/flood-plains"}
+          title={"Flood Plains"}
+          year={"2020"}
+          subtitle={"WebGL puzzle game made in Unity"}
+        />
+        <Card
+          image={wordBridgesImg}
+          link={"www.google.com"}
+          title={"Word Bridges"}
+          year={"2016"}
+          subtitle={"Word puzzle game made with Unity"}
+        />
+        <Card
+          image={logTablesImg}
+          link={
+            "https://play.google.com/store/apps/details?id=com.duck.log&hl=en_IE&gl=US"
+          }
+          year={"2012"}
+          title={"Log Tables App"}
+          subtitle={"Android app of log tables for Irish state examinations "}
+        />
+        <Card
+          image={formulaeImg}
+          link={
+            "https://play.google.com/store/apps/details?id=logtables.duckapps.logtablesv2&hl=en_IE&gl=US"
+          }
+          year={"2012"}
+          title={"Formulae and Tables"}
+          subtitle={"Recreation of the log tables app"}
+        />
+      </div>
     </div>
   );
 }
---
+++ src/Card.js
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function Card({ image, link, title, subtitle, year }) {
+  return (
+    <a className="card" href={link}>
+      <img className="image" src={image} alt={title} />
+      <div className="textContainer">
+        <div className="cardHeader">
+          <div className="title">{title}</div>
+          <div className="year">{year}</div>
+        </div>
+        <div className="subtitle">{subtitle}</div>
+      </div>
+    </a>
+  );
+}
+
+export default Card;
